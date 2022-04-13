763e2a8e58e531b6ad6a4d2b87134b0656832096
Tonylin <hsewa58468@gmail.com>
2022-04-13 14:35:17 +0800

    RESET

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
-import Body from './components/Body'
+
 
 export default class App extends Component {
   render() {
     return (
       <div >
-          <Body/>
+         
       </div>
     )
   }
