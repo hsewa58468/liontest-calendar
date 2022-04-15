77e336b09a6357df0f0fece9189582cecc8a9059
csdsdasdcwcwadx <jack0978330@gmail.com>
2022-04-15 13:48:34 +0800

    fix some conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React, { Component,Fragment } from 'react'
 import './App.css'
 import MonthBar from './components/Month_Bar/index'
-<<<<<<< HEAD
-=======
 import Neck from './components/Week'
->>>>>>> testA
 import Body from './components/Body'
 import { BrowserRouter } from 'react-router-dom';
 
