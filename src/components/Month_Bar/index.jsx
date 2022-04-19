6fc1bb7aa7983589ccea74750dcf406465332456
csdsdasdcwcwadx <jack0978330@gmail.com>
2022-04-19 17:47:06 +0800

    fix (some tab )

--- src/components/Month_Bar/index.jsx
+++ src/components/Month_Bar/index.jsx
@@ -8,6 +8,7 @@ export default function Month_Bar(){
 
     const[ChooseMonth,setChooseMonth]=useState({year:2017,month:7});
 
+    const[clickBar,setclickBar] = useState(2);
 
     function handlenext(){ 
        setChooseMonth(ChooseMonth=>({
@@ -31,22 +32,24 @@ export default function Month_Bar(){
 
 
     return(
-
+        
         <div className="calendars_tabWrap">
 
             <Link to={`/${ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year}/${ChooseMonth.month-1<1?12:ChooseMonth.month-1}`} onClick={()=>{handleprev()}} className="prev on"></Link>
             <ul className="ntb_tab">
 
                 <li className="tab">
-                    <Link to=""><span className=''>{ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year} {ChooseMonth.month-1<1?12:ChooseMonth.month-1}月</span></Link>
-                </li>&emsp; 
+                    <Link to=""><span className={clickBar===1? 'clickMonth' : ''} onClick={()=>setclickBar(1)}>{ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year} {ChooseMonth.month-1<1?12:ChooseMonth.month-1}月</span>
+                    </Link>
+                </li>
 
                 <li className="tab">
-                    <Link to=""><span className='clickMonth'>{ChooseMonth.year} {ChooseMonth.month}月</span></Link>
-                </li>&emsp;
+                    <Link to=""><span className={clickBar===2? 'clickMonth' : ''} onClick={()=>setclickBar(2)}>{ChooseMonth.year} {ChooseMonth.month}月</span></Link>
+                </li>
 
                 <li className="tab">
-                <Link to=""><span className=''>{ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year} {ChooseMonth.month+1>12?1:ChooseMonth.month+1}月</span></Link>
+                    <Link to=""><span className={clickBar===3? 'clickMonth' : ''} onClick={()=>setclickBar(3)} >{ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year} {ChooseMonth.month+1>12?1:ChooseMonth.month+1}月</span>
+                    </Link>
                 </li>
                                                                   
             </ul>
