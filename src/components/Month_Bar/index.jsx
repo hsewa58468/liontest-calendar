e5a04f707e1dbf121f8a2af6b0a4626edbebb709
csdsdasdcwcwadx <jack0978330@gmail.com>
2022-04-20 10:00:36 +0800

    fix (posible complete)

--- src/components/Month_Bar/index.jsx
+++ src/components/Month_Bar/index.jsx
@@ -7,6 +7,7 @@ import PubSub from 'pubsub-js';
 export default function Month_Bar(){
 
     const[ChooseMonth,setChooseMonth]=useState({year:2017,month:7});
+    const[tabMonth,settabMonth] = useState(1);
 
     function handlenext(){ 
        setChooseMonth(ChooseMonth=>({
@@ -34,19 +35,30 @@ export default function Month_Bar(){
         <div className="calendars_tabWrap">
 
             <Link to={`/${ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year}/${ChooseMonth.month-1<1?12:ChooseMonth.month-1}`} onClick={()=>{handleprev()}} className="prev on"></Link>
+
             <ul className="ntb_tab">
 
                 <li className="tab">
-                    <Link to=""><span className=''>{ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year} {ChooseMonth.month-1<1?12:ChooseMonth.month-1}月</span>
+                    <Link to={`/${ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year}/${ChooseMonth.month-1<1?12:ChooseMonth.month-1}`}><span className='' onClick={()=>{setChooseMonth(ChooseMonth=>({
+                            year:ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year,
+                            month:ChooseMonth.month-1<1?12:ChooseMonth.month-1
+                        }))}}>{ChooseMonth.month-1<1?ChooseMonth.year-1:ChooseMonth.year} {ChooseMonth.month-1<1?12:ChooseMonth.month-1}月</span>
                     </Link>
                 </li>
 
                 <li className="tab">
-                    <Link to=""><span className='clickMonth'>{ChooseMonth.year} {ChooseMonth.month}月</span></Link>
+                    <Link to={`/${ChooseMonth.year}/${ChooseMonth.month}`}><span className='clickMonth' onClick={()=>{setChooseMonth(ChooseMonth=>({
+                            year:ChooseMonth.year,
+                            month:ChooseMonth.month
+                        }))}}>{ChooseMonth.year} {ChooseMonth.month}月</span>
+                    </Link>
                 </li>
 
                 <li className="tab">
-                    <Link to=""><span className=''>{ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year} {ChooseMonth.month+1>12?1:ChooseMonth.month+1}月</span>
+                    <Link to={`/${ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year}/${ChooseMonth.month+1>12?1:ChooseMonth.month+1}`}><span className='' onClick={()=>{setChooseMonth(ChooseMonth=>({ 
+                            year:ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year,
+                            month:ChooseMonth.month+1>12?1:ChooseMonth.month+1
+                        }))}}>{ChooseMonth.month+1>12?ChooseMonth.year+1:ChooseMonth.year} {ChooseMonth.month+1>12?1:ChooseMonth.month+1}月</span>
                     </Link>
                 </li>
                                                                   
